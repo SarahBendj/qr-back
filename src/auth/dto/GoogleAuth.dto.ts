0f8bb3fe1a98150cdda06036c4ddf01d7ed832b5
SarahBendj <sarah.asma.bendjema@gmail.com>
2026-01-29 13:58:23 +0100

    test

--- src/auth/dto/GoogleAuth.dto.ts
+++ src/auth/dto/GoogleAuth.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsOptional, IsString, IsBoolean } from 'class-validator';
 
 export class GoogleLoginDto {
+  @IsString()
   id: string;
+
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsString()
   picture?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  userConsented?: boolean;
 }
